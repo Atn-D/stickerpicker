7ac2117a24a2c8de2d11618553fa777ab5d78969
AntoineÐ <ad-git@kher.nl>
2023-06-01 20:51:03 +0200

    update theme

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f59406a4'
-version = '0.1.0+dev.f59406a4'
-linkified_version = '0.1.0+dev.[f59406a4](https://github.com/maunium/stickerpicker/commit/f59406a47a6778cd402e656ffb64f667335f665a)'
+git_revision = 'f1b480c4'
+version = '0.1.0+dev.f1b480c4'
+linkified_version = '0.1.0+dev.[f1b480c4](https://github.com/maunium/stickerpicker/commit/f1b480c41ebf15b14cd31d51fb27c5a42c90abb6)'
