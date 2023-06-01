f30d49e0fb5b2775594a9acf3f09e793e420edba
AntoineÐ <ad-git@kher.nl>
2023-06-01 21:14:57 +0200

    update

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f59406a4'
-version = '0.1.0+dev.f59406a4'
-linkified_version = '0.1.0+dev.[f59406a4](https://github.com/maunium/stickerpicker/commit/f59406a47a6778cd402e656ffb64f667335f665a)'
+git_revision = '7ac2117a'
+version = '0.1.0+dev.7ac2117a'
+linkified_version = '0.1.0+dev.[7ac2117a](https://github.com/maunium/stickerpicker/commit/7ac2117a24a2c8de2d11618553fa777ab5d78969)'
--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f1b480c4'
-version = '0.1.0+dev.f1b480c4'
-linkified_version = '0.1.0+dev.[f1b480c4](https://github.com/maunium/stickerpicker/commit/f1b480c41ebf15b14cd31d51fb27c5a42c90abb6)'
+git_revision = '7ac2117a'
+version = '0.1.0+dev.7ac2117a'
+linkified_version = '0.1.0+dev.[7ac2117a](https://github.com/maunium/stickerpicker/commit/7ac2117a24a2c8de2d11618553fa777ab5d78969)'
